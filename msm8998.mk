b35575ba298046a69e2c0c30d8ac938fc04dbc27
Pranav Vashi <neobuddy89@gmail.com>
2021-05-08 21:49:13 +0530

    msm8998-common: Add hotword enrollment from taimen
    * Fix OK Google
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I5dcf73894050079c9493119befb017e6026b634c
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- msm8998.mk
+++ msm8998.mk
@@ -238,6 +238,9 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# Hotword enrollment
+$(call inherit-product, vendor/xiaomi/msm8998-common/google/google.mk)
+
 # IFAA manager
 PRODUCT_PACKAGES += \
     IFAAService
